919dc23ade9c25eb2bf693b8045769ae3f5f1ac6
Daniel <ds@danielstegen.de>
2020-02-28 22:58:15 +0100

    improved drag'n drop

--- public/schach.js
+++ public/schach.js
@@ -13,6 +13,7 @@ function setPosition (item) {
 var timeout = setTimeout(reloadBoard, 5000);
 function reloadBoard () {
   //$("#chessboard").load(location.href + " #chessboard");
+  $("#control").trigger("reset");
   location.reload();
   timeout = setTimeout(reloadBoard, 5000);
 }
@@ -33,22 +34,28 @@ $( function() {
 let oldP = '';
 let newP = '';
 $(".sortable").on("sortstart", function(event, ui) {
-  oldP = event.target.id;
-  console.log('oldPosition: '+event.target.id);
+  if (event.target.className.split(' ')[0] === currentPlayer) {
+    oldP = event.target.id;
+    console.log('oldPosition: '+event.target.id);
+  }
 });
 $(".sortable").on("sortreceive", function(event, ui) {
+  if (oldP !== '') {
     newP = event.target.id;
     console.log('newPosition: '+event.target.id);
+    $(':input[type="submit"]').prop('disabled', true);
     $.ajax({
-                url: '/action', // url where to submit the request
-                type : "GET", // type of action POST || GET
-                dataType : 'json', // data type
-                data : {"player": currentPlayer, "oldPosition": oldP, "newPosition": newP}, // post data || get data
-                success : function(result) {
-                    // you can see the result from the console
-                    // tab of the developer tools
-                    console.log(result);
-                }
-          });
-          timeout = setTimeout(reloadBoard, 1);
+      url: '/action', // url where to submit the request
+      type : "GET", // type of action POST || GET
+      dataType : 'json', // data type
+      data : {"player": currentPlayer, "oldPosition": oldP, "newPosition": newP}, // post data || get data
+        success : function(result) {
+        // you can see the result from the console
+        // tab of the developer tools
+        console.log(result);
+      }
+    });
+    $("control").trigger("reset");
+    timeout = setTimeout(reloadBoard, 10);
+  }
 });
