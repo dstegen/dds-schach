ed94c454e5588396edfd5f0588ff6c398b64b2fc
Daniel <ds@danielstegen.de>
2020-04-18 21:54:55 +0200

    code cleanup

--- index.js
+++ index.js
@@ -17,34 +17,3 @@ process.title = 'DDS-Schach';
 const server = new ServerDS('DDS-Schach');
 server.setCallback(router);
 server.startServer();
-
-
-/*
-const http = require('http');
-const WebSocket = require('ws');
-const {getIPs} = require('webapputils-ds');
-const router = require('./lib/router');
-
-let port = 8080;
-let wsport = 8080;
-let host = 'localhost';
-if (getIPs()['en0']) {
-	host = getIPs()['en0'];
-} else if (getIPs()['wlo1']) {
-	host = getIPs()['wlo1'];
-} else if (getIPs()['eth0']) {
-	host = getIPs()['eth0'];
-	wsport = 80;
-}
-console.log('Available network devices: ');
-console.log(getIPs());
-
-const server = http.createServer( function (request, response) {
-  router(request, response, wss, wsport);
-}).listen(port, host, () => console.log('DDS-Schach is online: http://'+host+':'+port+' (wsport:'+wsport+')'));
-
-const wss = new WebSocket.Server({
-	server,
-	clientTracking: true
-});
-*/
